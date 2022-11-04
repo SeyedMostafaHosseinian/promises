3976c01bfac7fa4cbda922c5447b56ee147f3e49
SeyedMostafaHosseinian <seyedmostafainsta786@gmail.com>
2022-11-04 11:43:43 +0330

    add async/await

--- main.js
+++ main.js
@@ -1,59 +1,69 @@
-// console.log('1')
-// setTimeout(() => {
-//     console.log('2')
-// },3000)
-// console.log('3')
 
 //use callback
-function getProduct(callback) {
-    let data = []
+function getProduct() {
+    let data = [];
     const BASE_URL = "https://digistore.glitch.me";
-    const xhr = new XMLHttpRequest()
-    xhr.open('GET',`${BASE_URL}/products`)
-    xhr.send()
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `${BASE_URL}/products`);
+    xhr.send();
     xhr.onload = () => {
-        data =JSON.parse(xhr.response)
-        callback(data)
-        console.log(xhr)
-
-
-    }
+        data = JSON.parse(xhr.response);
+        displayData(data);
+    };
 }
 function displayData(data) {
-    data.map(item => {
-        console.log(item)
-    })
+    console.log('callback', data[0]);
 }
-
+getProduct();
 ///// convert callbacks to promises
-function displayData2(data) {
-    data.map(item => {
-        console.log(item)
-    })
-}
-
 function getProduct2() {
-    return new Promise((resolve,reject) => {
-        let data = []
+    return new Promise((resolve, reject) => {
+        let data = [];
         const BASE_URL = "https://digistore.glitch.me";
-        const xhr = new XMLHttpRequest()
-        xhr.open('GET',`${BASE_URL}/products3`)
-        xhr.send()
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', `${BASE_URL}/products`);
+        xhr.send();
         xhr.onload = () => {
-            data =JSON.parse(xhr.response)
-            if (data.length) {
-                resolve(data)
+            data = JSON.parse(xhr.response);
+            if (data.length > 0) {
+                resolve(data);
             }
             else {
-                reject('error')
+                reject('error');
             }
-        }
-    } )
-
-
+        };
+    });
 }
-getProduct2().then(data => displayData2(data))
-    .catch(error => console.log('error'))
+function displayData2(data) {
+    console.log('promise', data[0]);
+}
+getProduct2()
+    .then(data => displayData2(data));
 
-
-
+/////convert promises to async/await
+function getProducts3() {
+    return new Promise((resolve, reject) => {
+        let data = [];
+        const BASE_URL = "https://digistore.glitch.me";
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', `${BASE_URL}/products`);
+        xhr.send();
+        xhr.onload = () => {
+            data = JSON.parse(xhr.response);
+            if (data.length > 0) {
+                resolve(data);
+            }
+            else {
+                reject('error');
+            }
+        };
+    });
+}
+function displayData3(data) {
+    console.log('async/await', data[0]);
+}
+async function logProducts3() {
+        const data = await getProducts3();
+        displayData3(data);
+}
+logProducts3();
